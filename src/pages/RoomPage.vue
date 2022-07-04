f80b9fcdc32e8477a218d77a1533e7b44f94e4b3
lowa <lowa@lowa>
2022-07-04 14:26:37 +0000

    restraunt

--- src/pages/RoomPage.vue
+++ src/pages/RoomPage.vue
@@ -6,10 +6,10 @@
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShnvg4B19hHctbPYKXLhetI-nMN2-2thc9XwVn7UQ6bQBiCXCxkY2QOoldc9Y5t0LQ77Q&usqp=CAU"
       />
       <q-card-actions vertical class="justify-around">
-          <div class="row no-wrap items-center">
-    <q-rating size="30px" v-model="stars" :max="5" color="blue" />
-    <span class="text-caption text-grey q-ml-sm">4.2 (551)</span>
-  </div>
+        <div class="row no-wrap items-center">
+          <q-rating size="30px" v-model="stars" :max="5" color="blue" />
+          <span class="text-caption text-grey q-ml-sm">4.2 (551)</span>
+        </div>
         <h2>ВЫСОТА 5642</h2>
         <q-text>Кавказская кухня</q-text>
         <q-text>ВРЕМЯ РАБОТЫ: Пн—Вс с 12:00 до 00:00</q-text>
