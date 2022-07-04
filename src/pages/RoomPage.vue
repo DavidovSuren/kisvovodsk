b3d1be289acd1e81d3d6216a2145f47d50938e9f
lowa <lowa@lowa>
2022-07-04 11:07:00 +0000

    foro

--- src/pages/RoomPage.vue
+++ src/pages/RoomPage.vue
@@ -1,7 +1,7 @@
 <template>
   <q-card class="my-card">
     <q-card-section horizontal>
-      <q-img class="col-5 flex flex-center" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+      <q-img class="col-5 flex flex-center" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShnvg4B19hHctbPYKXLhetI-nMN2-2thc9XwVn7UQ6bQBiCXCxkY2QOoldc9Y5t0LQ77Q&usqp=CAU"/>
       <q-card-actions vertical class="justify-around">
         <q-btn flat round color="red" icon="favorite" />
         <q-btn flat round color="accent" icon="bookmark" />
@@ -10,7 +10,7 @@
     </q-card-section>
   </q-card>
   <div class="row no-wrap items-center">
-    <q-rating size="18px" v-model="stars" :max="5" color="primary" />
+    <q-rating size="30px" v-model="stars" :max="5" color="blue" />
     <span class="text-caption text-grey q-ml-sm">4.2 (551)</span>
   </div>
 </template>
