e6e7d9b7debe4bf8ecc46d4b7d5588ba501bb695
lowa <lowa@lowa>
2022-07-04 09:48:51 +0000

    ROOM

--- src/pages/RoomPage.vue
+++ src/pages/RoomPage.vue
@@ -1,20 +1,16 @@
 <template>
-        <div class="row no-wrap items-center">
-          <q-rating size="18px" v-model="stars" :max="5" color="primary"/>
-          <span class="text-caption text-grey q-ml-sm">4.2 (551)</span>
-        </div>
+  <q-card class="my-card">
+    <q-card-section horizontal>
+      <q-img class="col" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+      <q-card-actions vertical class="justify-around">
+        <q-btn flat round color="red" icon="favorite" />
+        <q-btn flat round color="accent" icon="bookmark" />
+        <q-btn flat round color="primary" icon="share" />
+      </q-card-actions>
+    </q-card-section>
+  </q-card>
+  <div class="row no-wrap items-center">
+    <q-rating size="18px" v-model="stars" :max="5" color="primary" />
+    <span class="text-caption text-grey q-ml-sm">4.2 (551)</span>
+  </div>
 </template>
-
-<style lang="sass" scoped>
-.shadow-box
-  width: 90px
-  height: 90px
-  margin: 25px
-  border-radius: 50%
-  font-size: 12px
-.doc-inset-shadow
-  width: 120px
-  height: 120px
-  border: 1px solid #eee
-  padding: 4px
-</style>
