de337ce2517558efdaaad90f43d11d3989157efc
lowa <lowa@lowa>
2022-07-07 15:08:17 +0000

    txt

---
+++ dist/pwa/assets/InfoPage.72ededd8.js
@@ -0,0 +1,1 @@
+import{Q as a}from"./QPage.ad50558c.js";import{_ as e,V as o,W as r}from"./index.00680462.js";const n={name:"InfoPage"};function t(s,c,p,f,_,d){return o(),r(a,{padding:""})}var g=e(n,[["render",t]]);export{g as default};
