b4bd0d9f2fc8512de34c9d6139ee91f333d51580
lowa <lowa@lowa>
2022-07-07 15:07:54 +0000

    -

--- dist/pwa/assets/InfoPage.3e21d0d2.js
+++
@@ -1,1 +0,0 @@
-import{Q as a}from"./QPage.3e5e29c8.js";import{_ as e,V as o,W as r}from"./index.9dbf9ad1.js";const n={name:"InfoPage"};function t(s,c,p,f,_,d){return o(),r(a,{padding:""})}var g=e(n,[["render",t]]);export{g as default};
