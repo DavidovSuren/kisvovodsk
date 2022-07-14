f250d6701a658918b6506aba8890098e58cdda8d
goshva <sha.egor@gmail.com>
2022-07-14 17:57:45 +0300

    build del

--- dist/pwa/assets/ClothingPage.dd4b82b1.js
+++
@@ -1,1 +0,0 @@
-import{Q as a}from"./QPage.005bafa2.js";import{_ as e,V as o,W as r}from"./index.ab538d42.js";const t={};function s(c,n,p,_,f,i){return o(),r(a,{padding:""})}var g=e(t,[["render",s]]);export{g as default};
--- src/pages/InfoPage.vue
+++ src/pages/InfoPage.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <!-- content -->о нас
   </q-page>
 </template>
 <script>
