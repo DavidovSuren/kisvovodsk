e006d52ac045030e0a13538091e45afe471562b6
lowa <lowa@lowa>
2022-07-18 16:32:35 +0000

    build

--- src/pages/PostPage.vue
+++ src/pages/PostPage.vue
@@ -46,8 +46,25 @@ onMounted(() => {
     </q-card>
   </q-page>
 </template>
-<style lang="sass" scoped>
-.my-card
-  width: 100%
-  max-width: 100%
+<style>
+.wp-block-image {
+  width: 100% !important;
+}
+.wp-block-image * {
+  margin: 0 !important;
+  padding: 0 !important;
+}
+figure {
+  width: 100% !important;
+  margin-block-start: 0 !important;
+  margin-block-end: 0 !important;
+  margin-inline-start: 0 !important;
+  margin-inline-end: 0 !important;
+}
+.wp-block-image img {
+  width: 100% !important;
+  height: auto !important;
+  max-width: 100% !important;
+  min-width: 100% !important;
+}
 </style>
