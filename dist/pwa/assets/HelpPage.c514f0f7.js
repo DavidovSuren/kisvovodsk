d8ce0e026b198c3c3650c51b2cc84723fcb394c5
lowa <lowa@lowa>
2022-07-26 14:04:10 +0000

    build

--- dist/pwa/assets/HelpPage.c514f0f7.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,V as a,a2 as r}from"./index.1773819e.js";const t={name:"HelpPage"};function n(o,c,p,s,_,l){return a(),r("p",null,"ddd")}var f=e(t,[["render",n]]);export{f as default};
--- src/pages/HelpPage.vue
+++ src/pages/HelpPage.vue
@@ -1,6 +1,13 @@
 <template>
-  <p>ddd</p>
+  <div>
+    <form id="my-form" action="https://formspree.io/f/mknypyer" method="POST">
+      <label style="color: #fdfcfd">Email:</label> <br />
+      <input class="field" type="email" name="email" /> <br />
+      <label style="color: #fdfcfd">Запись на консультацию:</label> <br />
+      <textarea class="field" type="text" name="message"></textarea>
+      <input type="submit" class="field" id="my-form-button" />
+      <p id="my-form-status"></p>
+      <br />
+    </form>
+  </div>
 </template>
-<script>
-export default { name: 'HelpPage' }
-</script>
