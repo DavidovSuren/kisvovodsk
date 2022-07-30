5cef77170b01d48d511a281d3e70a804c4f25b1e
lowa <lowa@lowa>
2022-07-30 11:26:56 +0000

    build van

--- dist/pwa/assets/i18n.8bfcb8a1.js
+++ dist/pwa/assets/i18n.8bfcb8a1.js
@@ -1,4 +1,4 @@
-import{h as $,F as xe,i as kt,r as G,g as $e,c as B,w as Fe,a as pt,o as wt,b as St,d as ve,T as ke,e as Ot}from"./index.fb1992e9.js";/*!
+import{h as $,F as xe,i as kt,r as G,g as $e,c as B,w as Fe,a as pt,o as wt,b as St,d as ve,T as ke,e as Ot}from"./index.dee29d0b.js";/*!
   * @intlify/shared v9.1.10
   * (c) 2022 kazuya kawaguchi
   * Released under the MIT License.
