e475d9c1b323c603ff577fb6d7d433b2a42ea45a
lowa <lowa@lowa>
2022-08-09 15:15:54 +0000

    build ковычки

--- dist/pwa/assets/i18n.cc96d1cb.js
+++ dist/pwa/assets/i18n.cc96d1cb.js
@@ -1,4 +1,4 @@
-import{h as $,F as xe,i as kt,r as G,g as $e,c as B,w as Fe,a as pt,o as wt,b as St,d as ve,T as ke,e as Ot}from"./index.3436a26d.js";/*!
+import{h as $,F as xe,i as kt,r as G,g as $e,c as B,w as Fe,a as pt,o as wt,b as St,d as ve,T as ke,e as Ot}from"./index.89c35359.js";/*!
   * @intlify/shared v9.1.10
   * (c) 2022 kazuya kawaguchi
   * Released under the MIT License.
