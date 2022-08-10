262eba07c3e0f2eabfa9a32d3d40efe8c52d2541
lowa <lowa@lowa>
2022-08-10 10:04:38 +0000

    build youtube

--- src/pages/VideoPage.vue
+++ src/pages/VideoPage.vue
@@ -8,4 +8,14 @@
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
     allowfullscreen
   ></iframe>
+  <br>
+  <iframe
+    width="100%"
+    height="400"
+    src="https://www.youtube.com/embed/Q-o9G9qeabo"
+    title="YouTube video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  ></iframe>
 </template>
