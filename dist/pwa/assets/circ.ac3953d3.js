7c0b3c70195ac7ec5e1a7d6ffc45e2167f7b6c93
lowa <lowa@lowa>
2022-08-16 13:54:01 +0000

    build

--- dist/pwa/assets/circ.ac3953d3.js
+++
@@ -1,1 +0,0 @@
-var s="/assets/lyg.9ccd7984.jpeg",a="/assets/circ.d2111a26.jpeg";export{s as _,a};
--- dist/pwa/assets/use-ratio.ee52eb05.js
+++
@@ -1,1 +0,0 @@
-import{c as i}from"./index.61739497.js";const e={ratio:[String,Number]};function a(r,t){return i(()=>{const o=Number(r.ratio||(t!==void 0?t.value:void 0));return isNaN(o)!==!0&&o>0?{paddingBottom:`${100/o}%`}:null})}export{a,e as u};
