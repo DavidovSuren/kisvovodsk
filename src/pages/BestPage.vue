5b7850200477456f2f4d917580a1b9d3ed36ff99
lowa <lowa@lowa>
2022-08-16 11:39:09 +0000

    best

--- src/pages/BestPage.vue
+++ src/pages/BestPage.vue
@@ -1,3 +1,87 @@
 <template>
-<h1>ЛУЧШЕЕ</h1>
+  <div>
+    <q-splitter v-model="splitterModel" style="height: 450px">
+      <template v-slot:before>
+        <div class="q-pa-md">
+          <q-date v-model="date" :events="events" event-color="orange" />
+        </div>
+      </template>
+      <template v-slot:after>
+        <q-tab-panels
+          v-model="date"
+          animated
+          transition-prev="jump-up"
+          transition-next="jump-up"
+        >
+          <q-tab-panel name="2022/08/17">
+            <div class="text-h4 q-mb-md">2022/08/17</div>
+            <p>
+              Как и обещали, концерты классической музыки под открытым небом на проспекте Ленина продолжаются.
+              Сегодня с 18.00 до 20.30 выступают преподаватели Детской музыкальной школы имени Рахманинова.
+              Пианино и саксофон
+              Добро пожаловать в классику курортов - в Кисловодск!.
+            </p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+              praesentium cumque magnam odio iure quidem, quod illum numquam
+              possimus obcaecati commodi minima assumenda consectetur culpa fuga
+              nulla ullam. In, libero.
+            </p>
+          </q-tab-panel>
+
+          <q-tab-panel name="2022/08/18">
+            <div class="text-h4 q-mb-md">2022/08/18</div>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+              praesentium cumque magnam odio iure quidem, quod illum numquam
+              possimus obcaecati commodi minima assumenda consectetur culpa fuga
+              nulla ullam. In, libero.
+            </p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+              praesentium cumque magnam odio iure quidem, quod illum numquam
+              possimus obcaecati commodi minima assumenda consectetur culpa fuga
+              nulla ullam. In, libero.
+            </p>
+          </q-tab-panel>
+
+          <q-tab-panel name="2022/08/19">
+            <div class="text-h4 q-mb-md">2022/08/19</div>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+              praesentium cumque magnam odio iure quidem, quod illum numquam
+              possimus obcaecati commodi minima assumenda consectetur culpa fuga
+              nulla ullam. In, libero.
+            </p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+              praesentium cumque magnam odio iure quidem, quod illum numquam
+              possimus obcaecati commodi minima assumenda consectetur culpa fuga
+              nulla ullam. In, libero.
+            </p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+              praesentium cumque magnam odio iure quidem, quod illum numquam
+              possimus obcaecati commodi minima assumenda consectetur culpa fuga
+              nulla ullam. In, libero.
+            </p>
+          </q-tab-panel>
+        </q-tab-panels>
+      </template>
+    </q-splitter>
+  </div>
 </template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      splitterModel: ref(50),
+      date: ref('2022/08/17'),
+      events: ['2022/08/17', '2022/08/18', '2022/08/19']
+    }
+  }
+}
+</script>
