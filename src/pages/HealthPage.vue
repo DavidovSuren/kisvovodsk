d90b013648e2aac20cfc85846ee43f97fdf56014
lowa <lowa@lowa>
2022-08-23 07:01:08 +0000

    fast

--- src/pages/HealthPage.vue
+++ src/pages/HealthPage.vue
@@ -2,28 +2,28 @@
   <div class="q-pa-md">
     <div>
       <q-card >
-        <q-img style="height:135px" src="../assets/banerBest/restBan.jpeg" @click="this.$router.push('/restraunt'); ">
+        <q-img style="height:160px" src="../assets/banerBest/restBan.jpeg" @click="this.$router.push('/restraunt'); ">
         <q-card-section><h2 class="aLine">РЕСТОРАНЫ</h2></q-card-section>
         </q-img>
       </q-card>
     </div>
     <div class="mtt">
       <q-card>
-        <q-img style="height:135px" src="../assets/banerBest/cafeBan.jpeg" @click="this.$router.push('/cafe'); ">
+        <q-img style="height:160px" src="../assets/banerBest/cafeBan.jpeg" @click="this.$router.push('/cafe'); ">
     <q-card-section><h2 class="aLine">КАФЕ</h2></q-card-section>
         </q-img>
       </q-card>
     </div>
     <div class="mtt">
       <q-card>
-        <q-img style="height:135px" src="../assets/banerBest/stolBan.jpg" @click="this.$router.push('/canteen'); ">
+        <q-img style="height:160px" src="../assets/banerBest/stolBan.jpg" @click="this.$router.push('/canteen'); ">
           <q-card-section><h2 class="aLine">СТОЛОВЫЕ</h2></q-card-section>
         </q-img>
       </q-card>
     </div>
     <div class="mtt">
       <q-card>
-        <q-img style="height:135px" src="../assets/banerBest/fastban.webp" @click="this.$router.push('/fastfood'); ">
+        <q-img style="height:160px" src="../assets/banerBest/fastBan.jpg" @click="this.$router.push('/fastfood'); ">
                <q-card-section><h2 class="aLine">ФАСТ-ФУД</h2></q-card-section>
         </q-img>
       </q-card>
