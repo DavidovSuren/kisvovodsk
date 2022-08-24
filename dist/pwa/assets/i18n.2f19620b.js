84b35075e818dd1eef923a2edf77d4031235f92b
lowa <lowa@lowa>
2022-08-24 11:40:52 +0000

    menu

--- dist/pwa/assets/i18n.2f19620b.js
+++ dist/pwa/assets/i18n.2f19620b.js
@@ -1,4 +1,4 @@
-import{h as $,F as xe,i as kt,r as G,g as $e,c as B,w as Fe,a as pt,o as wt,b as St,d as ve,T as ke,e as Ot}from"./index.f1212022.js";/*!
+import{h as $,F as xe,i as kt,r as G,g as $e,c as B,w as Fe,a as pt,o as wt,b as St,d as ve,T as ke,e as Ot}from"./index.02524f12.js";/*!
   * @intlify/shared v9.1.10
   * (c) 2022 kazuya kawaguchi
   * Released under the MIT License.
