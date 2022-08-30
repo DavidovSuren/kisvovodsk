b8cfef977227ab431a7d5f333cb1297e4959756e
r2r1 <grishaivan8@gmail.com>
2022-08-31 00:12:26 +0300

    help

--- src/pages/HelpPage.vue
+++ src/pages/HelpPage.vue
@@ -1,32 +1,73 @@
 <template>
-  <div class="q-pa-md row items-start q-gutter-md">
-   <q-card
-      class="my-card text-white telegram-link"
-    >
-      <q-card-section style="text-align:center">
-      <div class="text-h4">ТЕЛЕГРАМ</div><br>
+         <h1 class="contentTitle">Связь для помощи</h1>
+         <q-separator class="separ" inset color="white" />
+  <div class="q-pa-md ">
+   <q-card class="q-pa-md">
          <q-btn
-          dense
-          icon="fa-brands fa-telegram"
-          aria-label="Fa-brands fa-telegram"
-          color="blue"
-          size="3em"
-          :href="'https://t.me/heart_city_kissloveodsk'"
-
-        />
-      </q-card-section>
+         class="full-width"
+            push
+            dense
+            icon="fa-brands fa-vk"
+            aria-label="Fa-brands fa-vk"
+            color="positive"
+            size="40px"
+            href="https://vk.com/heart_city_kissloveodsk"
+          />
+    </q-card>
+       <q-card class="q-pa-md">
+         <q-btn
+         class="full-width"
+            push
+            dense
+            icon="telegram"
+            aria-label="Fa-brands fa-vk"
+            color="positive"
+            size="40px"
+            href="https://t.me/heart_city_kissloveodsk "
+          />
+    </q-card>
+           <q-card class="q-pa-md">
+         <q-btn
+         class="full-width"
+            push
+            dense
+            icon="email"
+            aria-label="Email"
+            color="positive"
+            size="40px"
+            href="Davidov05101996@gmail.com "
+          />
+    </q-card>
+           <q-card class="q-pa-md">
+         <q-btn
+         class="full-width"
+            push
+            dense
+            icon="phone"
+            aria-label="Fa-brands fa-vk"
+            color="positive"
+            size="40px"
+            href="`tel:${+79844444068}`"
+          />
     </q-card>
   </div>
 </template>
 <script>
 export default { name: 'InfoPage' }
 </script>
-<style lang="sass" scoped>
-.my-card
-  width: 100%
-  max-width: 100%
-  margin-top: 80%
-.telegram-link
-  background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)
-  border-radius: 10px 150px / 120px
+<style  scoped>
+.q-card{
+  background: none;
+  box-shadow: none;
+}
+.contentTitle {
+  text-align: center;
+  padding-top: 15px;
+  margin-bottom: -40px;
+  font-size: 27px;
+  color: white;
+}
+  .separ {
+  margin-top: 30px;
+}
 </style>
