6f0a4caa6e84babc4ae96bd7ba97fc87bee808a4
lowa <lowa@lowa>
2022-09-07 14:58:45 +0000

    бууилд

--- dist/pwa/assets/QCardSection.9aaac2a4.js
+++
@@ -1,1 +0,0 @@
-import{f as e,c as r,h as n,j as s}from"./index.b20cc2a3.js";var i=e({name:"QCardSection",props:{tag:{type:String,default:"div"},horizontal:Boolean},setup(t,{slots:a}){const o=r(()=>`q-card__section q-card__section--${t.horizontal===!0?"horiz row no-wrap":"vert"}`);return()=>n(t.tag,{class:o.value},s(a.default))}});export{i as Q};
---
+++ dist/pwa/assets/use-quasar.f2849182.js
@@ -0,0 +1,1 @@
+import{a,at as r}from"./index.6b26447d.js";function t(){return a(r)}export{t as u};
