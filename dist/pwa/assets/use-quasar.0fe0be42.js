1f31bd4574d63be68bbd7d529ac02095dab2065b
goshva <segor@rhino-digital.com>
2022-09-12 11:32:59 +0300

    build ya info fix

--- dist/pwa/assets/use-quasar.0fe0be42.js
+++
@@ -1,1 +0,0 @@
-import{a,at as r}from"./index.fc89c5e4.js";function t(){return a(r)}export{t as u};
---
+++ dist/spa/assets/use-quasar.77901b66.js
@@ -0,0 +1,1 @@
+import{a,at as r}from"./index.59e2e208.js";function t(){return a(r)}export{t as u};
