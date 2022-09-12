3ccdb0d9765541288a94105b59d94a6ec2193607
goshva <segor@rhino-digital.com>
2022-09-12 13:45:34 +0300

    build fix centred informer

--- dist/pwa/assets/use-quasar.3063e2eb.js
+++
@@ -1,1 +0,0 @@
-import{a,at as r}from"./index.708e5225.js";function t(){return a(r)}export{t as u};
--- src/components/yandexInfo.vue
+++ src/components/yandexInfo.vue
@@ -1,9 +1,11 @@
 <template>
-<!-- Yandex.Metrika informer -->
-<a href="https://metrika.yandex.ru/stat/?id=89683386&amp;from=informer"
-target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/89683386/3_0_ECECD0FF_ECECD0FF_0_pageviews"
-style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" /></a>
-<!-- /Yandex.Metrika informer -->
+  <div style="    display: flex;     justify-content: center;"></div>
+    <!-- Yandex.Metrika informer -->
+    <a href="https://metrika.yandex.ru/stat/?id=89683386&amp;from=informer"
+    target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/89683386/3_0_ECECD0FF_ECECD0FF_0_pageviews"
+    style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" /></a>
+    <!-- /Yandex.Metrika informer -->
+  </div>
     <!--
       :TODO try to use data from https://mc.yandex.ru/informer/89683386/json
       or https://mc.yandex.ru/informer/89683386/json?callback=ym1323965510316
