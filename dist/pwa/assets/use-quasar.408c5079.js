ef40fcdef26d65885aa9d7a5401b93663500f7b1
goshva <sha.egor@gmail.com>
2022-09-20 09:18:15 +0300

    build whith menu

---
+++ dist/pwa/assets/use-quasar.408c5079.js
@@ -0,0 +1,1 @@
+import{a,au as r}from"./index.8c5715e3.js";function s(){return a(r)}export{s as u};
--- dist/pwa/assets/use-quasar.93b1fe58.js
+++
@@ -1,1 +0,0 @@
-import{a,au as r}from"./index.de5a0afc.js";function s(){return a(r)}export{s as u};
